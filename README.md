6a5b6f25aba2600ede834744ddbc74b5b35e9782
Dija <145768761+dija-64@users.noreply.github.com>
2024-01-20 12:16:43 +0000

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Challenge-5-Password-Gen
-A repository for the Module 5 Challenge, JavaScript Password Generator 
+A repository for the Module 5 Challenge, JavaScript Random Password Generator 
+
+# Installation
+
+Accessible via GitHub Pages
+
+# Usage
+
+Click the 'Generate Button' to randomly generate a secure password including uppercase, lowercase, numerical and special characters. Password must be between 8 and 128 characters long.
+
+# Credits
+© 2024 edX Boot Camps LLC 
+freecodecamp.org
+Open AI + Chat GPT for bug solving
+
+# License 
+
+Please refer to the LICENSE.txt - MIT LICENSE
--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,4 @@
-function generatePassword() {
+function generateRandomPassword() {
   // Array of special characters to be included in password
   var specialCharacters = [
     '@',
@@ -27,87 +27,33 @@ function generatePassword() {
   ];
 
   // Array of numeric characters to be included in password
-  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var numericCharacters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
   // Array of lowercase characters to be included in password
-  var lowerCasedCharacters = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z'
-  ];
+  var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
   // Array of uppercase characters to be included in password
-  var upperCasedCharacters = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z'
-  ];
+  var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 
-  // Function to prompt user for password options
-  function getPasswordOptions() {
-    var passwordLength = parseInt(prompt("Enter password. Your password must be between 8 - 128 characters long:"));
-    if (isNaN(passwordLength)) {
-      alert("Please input a password.");
-      return;
-    } else if (passwordLength < 8 || passwordLength > 128) {
-      alert("Password must be between 8 - 128 characters long.");
-      return;
-    };
+  // Function to prompt user for password options + length check
+  let passwordLength = parseInt(prompt("Enter password. Your password must be between 8 - 128 characters long:"));
+
+  if (isNaN(passwordLength)) {
+    alert("Please input password.");
+    return;
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 - 128 characters long.");
+    return;
   }
 
 
+
   // Prompt for charater types in password
-  const addSpecialChar = confirm("Would you like to include special characters?");
-  const addNumbers = confirm("Would you like to include numeric characters?");
-  const addLowercase = confirm("Would you like to include lowercase letters?");
-  const addUppercase = confirm("Would you like to include uppercase letters?");
+  const addSpecialChar = confirm("Would you like to include special characters in your password?");
+  const addNumbers = confirm("Would you like to include numeric characters in your password?");
+  const addLowercase = confirm("Would you like to include lowercase letters in your password?");
+  const addUppercase = confirm("Would you like to include uppercase letters in your password?");
 
   if (!addSpecialChar && !addNumbers && !addLowercase && !addUppercase) {
     alert("Please select at least one type of character.");
@@ -121,21 +67,23 @@ function generatePassword() {
   if (addUppercase) allChars += upperCasedCharacters
 
 
-  // Get references to the #generate element
-  var generateBtn = document.querySelector('#generate');
-
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector('#password');
-    if (password) {
-      passwordText.value = password;
-    } else {
-      passwordText.value = " ";
-    }
+  let newPassword = "";
+  for (let i = 0; i < newPasswordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    newPassword += allCharacters.charAt(randomIndex);
   }
+
+  return password;
 
 }
 
+const genPassword = generateRandomPassword();
+if (genPassword) {
+  alert("Your new, randomy generated password is: " + password)
+
+}
+
+
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+var generateBtn = document.querySelector('#generate');
+generateBtn.addEventListener('click', generateRandomPassword);
